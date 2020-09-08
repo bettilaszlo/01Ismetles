eafbd690eb95c7b56cb38718136d640e875099a2
bettilaszlo <laszlobetti3@gmail.com>
2020-09-08 11:22:10 +0200

    Felhasználó választ

--- 01Ismetles/Program.cs
+++ 01Ismetles/Program.cs
@@ -11,6 +11,12 @@ namespace _01Ismetles
             int gepvalasz = vel.Next(0, 3);
             Console.WriteLine("Gép választása: {0}", lehetoseg[gepvalasz]);
 
+            int jatekosvalasz;
+            Console.WriteLine("kő (0), papír (1), olló (2)");
+            Console.Write("Válassz: ");
+            jatekosvalasz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Játékos választása: {0}", lehetoseg[jatekosvalasz]);
+
             Console.ReadKey();
         }
     }
